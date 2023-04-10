5c20e0b64c28ab524ba9ccd0f7066c8ba9ef7f72
Maria Villalobos <mariaelevillalcortes@gmail.com>
2023-04-09 20:56:51 -0600

    Expresion regular movido a una variable

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
   public getId: number = 0;
   public getIdCurp: string = '';
   public curp: string = '';
+  public vCurp: string = '([A-Z][AEIOUX][A-Z]{2}\\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\\d])(\\d)$';
   public mostrar: boolean = true;
   public mostrarHC: boolean = true;
   
@@ -82,4 +83,6 @@ export class HomeComponent implements OnInit {
     //Dar
     this.dataService.mostrarHEvent$.emit(this.mostrar);
    }
+
+   
 }
